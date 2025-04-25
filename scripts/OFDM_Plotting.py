39cb9c73883ff97e026b4ca45fc5e727760f6e66
Unalome81 <vivaswan21217@iiitd.ac.in>
2025-04-26 01:25:49 +0530

    Unnecessary Folder Removed

--- scripts/OFDM_Plotting.py
+++ scripts/OFDM_Plotting.py
@@ -27,7 +27,7 @@ def read_double_file(filename):
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(BASE_DIR, "data")
-RESULTS_DIR = os.path.join(BASE_DIR, "results", "plots")
+RESULTS_DIR = os.path.join(BASE_DIR, "results")
 
 os.makedirs(RESULTS_DIR, exist_ok=True)
 
